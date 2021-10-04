e8136ba8ed631077cb33dca0305d1134ab7ee07d
Juan I. Lanosa <blackmoodgames@gmail.com>
2021-10-03 23:42:52 -0300

    health bar player

--- Assets/_scripts/systems/health_system/HealthBar.cs
+++ Assets/_scripts/systems/health_system/HealthBar.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class HealthBar : MonoBehaviour
 {
-    private Health healthSys;
+    protected Health healthSys;
 
-    private Transform lifeBarFill;
+    protected Transform lifeBarFill;
 
     public void Start()
     {
         this.lifeBarFill = transform.Find("Bar");
+        
     }
 
-    public void SetUpLifeBar(Health hRef)
+    public virtual void SetUpLifeBar(Health hRef)
     {
         this.healthSys = hRef.SetHealthChangedHandler(this.OnHealthChanged);
     }
-    public void OnHealthChanged()
+    public virtual void OnHealthChanged()
     {
         this.lifeBarFill.localScale = new Vector3(this.healthSys.GetHealthPercentaje(), 1);
     }
---
+++ Assets/_scripts/systems/health_system/HealthBarUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarUI : HealthBar
+{
+    [SerializeField] private Slider slider;
+
+    public override void SetUpLifeBar(Health hRef)
+    {
+        base.SetUpLifeBar(hRef);
+
+        this.slider.maxValue = 1;
+        this.slider.value = hRef.GetHealthPercentaje();
+    }
+
+    public override void OnHealthChanged()
+    {
+        this.slider.value = this.healthSys.GetHealthPercentaje();
+    }
+}
